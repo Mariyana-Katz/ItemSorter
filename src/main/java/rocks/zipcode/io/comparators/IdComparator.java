ff89280dccf926af683c7fd35eb0b55e9fca5191
Mariyana-Katz <mariqnak21@gmail.com>
2024-04-01 15:40:54 -0400

    finished

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.function.ToLongFunction;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+      if(o1.getId().compareTo(o2.getId()) != 0){
+          return o1.getId().compareTo((o2.getId()));
+        } else{
+        return o1.getId().compareTo(o2.getId());
+      }
+    }
+
 }
